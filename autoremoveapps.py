04c4cebf0017ef58bedddaeba14cf1bbe78867f7
kulkarnip150 <39945169+kulkarnip150@users.noreply.github.com>
2020-08-29 18:46:10 +0530

    Add files via upload

---
+++ autoremoveapps.py
@@ -0,0 +1,24 @@
+import time
+import pyautogui
+import os
+
+startTime = time.time()
+with open("debloatmiui.txt") as f:
+    lines = [line.rstrip() for line in f]
+# print(lines)
+time.sleep(1)
+os.system("start")
+time.sleep(2)
+pyautogui.write("adb devices", interval=0.02)
+pyautogui.press("enter")
+time.sleep(1)
+pyautogui.write("adb shell", interval=0.02)
+pyautogui.press("enter")
+for i in lines:
+    time.sleep(2)
+    print(i)
+    pyautogui.write(i, interval=0.01)
+    pyautogui.press("enter")
+
+endTime = time.time()
+print(f"Total time {endTime - startTime}")
