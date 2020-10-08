8bb54565d173165554ee2ba352f4adacb419effd
kulkarnip150 <39945169+kulkarnip150@users.noreply.github.com>
2020-10-08 13:01:36 +0530

    Update autoremoveapps.ps1

--- autoremoveapps.ps1
+++ autoremoveapps.ps1
@@ -1,10 +1,10 @@
-$array = Get-Content -Path @("apps.txt")
+$array = Get-Content -Path @("debloatmiui.txt")
 $count=$array.Length
 write-host "Near about "$count " apps are going to remove " -ForegroundColor red -BackgroundColor Yellow
 write-host ""
-write-host "Allow USB debugging on devices" -ForegroundColor Yellow -BackgroundColor red
+write-host "Allow USB debugging on device" -ForegroundColor Yellow -BackgroundColor red
 write-host ""
-write-host "waiting for 5 sec" -ForegroundColor Yellow -BackgroundColor red
+write-host "waiting for 5 seconds" -ForegroundColor Yellow -BackgroundColor red
 write-host ""
 Start-Sleep -s 3
 adb devices
@@ -18,7 +18,6 @@ foreach($item in $array)
     Invoke-Expression $command
     write-host ""
 }
-adb install VLC.apk
 Start-Sleep -s 2
 write-host "Device will restart..." -ForegroundColor red -BackgroundColor Yellow
 adb reboot
